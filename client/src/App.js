1c1c0cf91af18eab81b26d3741d42fcb12e1c458
Daniel Nguyen <nguyendaniel1997@gmail.com>
2024-03-02 09:32:31 -0600

    Created userContext

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
 import './App.css';
-import Authentication from './components/authentication/authentication'
-import Chat from './components/chat/chat'
+import Authentication from './components/authentication/authentication';
+import Chat from './components/chat/chat';
+import { UserProvider } from './components/userContext';
 
 function App() {
   return (
-    <Router>
+    <UserProvider>
+      <Router>
         <Routes>
             <Route path="/" exact element={<Authentication />} />
             <Route path="/chat" element={<Chat />} />
         </Routes>
-    </Router>
+      </Router>
+    </UserProvider>
   );
 }
 
